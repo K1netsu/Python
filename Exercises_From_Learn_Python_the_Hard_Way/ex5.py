68827392869fd39e588a9a44bdda83e10cfc36b3
Enzo Vinícius <89085302+K1netsu@users.noreply.github.com>
2023-12-26 16:01:25 -0300

    Add files via upload

---
+++ Exercises_From_Learn_Python_the_Hard_Way/ex5.py
@@ -0,0 +1,17 @@
+myName = 'Enzo'
+myAge = 19
+myHeight = 1.83 # metros
+myWeight = 70 # kilos
+myEyes = 'Black'
+myTeeth = 'White'
+myHair = 'Brown'
+
+print(f"Let's talk about {myName}.")
+print(f"He's {myHeight} meters tall.")
+print(f"He's {myWeight} kg heavy.")
+print("Actually thats not too heavy.")
+print(f"He's got {myEyes} eyes and {myHair} hair.")
+print(f"His teeth are usually {myTeeth} dependig on the coffee.")
+
+total = myAge + myHeight + myWeight
+print(f"If i add {myAge}, {myHeight}, and {myWeight} I get {total}.")
